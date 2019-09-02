02bfe0ee06d8f60102e7785f00337e506daf468f
ferdisaltek <ferdisaltek@gmail.com>
2019-09-02 18:48:40 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {FirstComponent} from './first_comp';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <FirstComponent>this is my fisrt component</FirstComponent>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/first_comp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export class FirstComponent extends React.Component{
+    render(){
+        return(
+            <div>this is my first Component</div>
+        );
+    }
+}
