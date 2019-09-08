d5bf2781999b7c55f1b7bf9141baf493d406c13b
ferdisaltek <ferdisaltek@gmail.com>
2019-09-08 16:25:40 +0300

    state_begin

--- src/User.js
+++ src/User.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 export default class User extends Component {
+constructor (props){
+    super(props);
+
+    this.state={
+        test :"TEST"
+    }
+}
+
     render() {
         return (
             <div>
                 <form>
                     <input type="text"></input>
                     <button>gönder</button>
+                    <p>{this.state.test}</p>
                 </form>
             </div>
         )
